5d50a491a157e9af6ee634f8ee25628eaf682bff
Leedonggyu <rb2978@naver.com>
2018-06-21 08:24:07 +0000

    read

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :user_signed_in?
+  
+  #현재 로그인 된 상태니?
+  #로그인 되어 있지 않으면 로그인하는 페이지로 이동 시켜줘
+  def user_signed_in?
+    session[:current_user].present?
+  end
+  #현재 로그인 된 사람이 누구니?
+  def authenticate_user! #원하지않는곳으로 이동하는것을 막기위해 !를 사용한다.
+    unless user_signed_in?
+        redirect_to '/sign_in'
+    end
+  end
+  def current_user
+    #현재 로그인됐니?
+    if user_signed_in?
+        #됐다면 로그인 한 사람은 누구니?
+        @current_user = User.find(session[:current_user])
+    end
+  end
 end
--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,9 +1,17 @@
 class BoardController < ApplicationController
+  before_action :set_post,only: [:show,:edit,:update,:destroy]
+
+  #before_action :set_post,except: [:index,:new,:create]
+  #로그인 된 상태에서만 접속할 수 있는 페이지는?
+  #index,show만 로그인 하지 않은 상태에서 볼 수 있게
+  #나머지는 반드시 로그인이 필요하게
+  before_action :authenticate_user!, except: [:index,:show]
+
   def index
     @posts= Post.all
+    @current_user =current_user
   end
   def show
-    @post = Post.find(params[:id])
   end
   def new
   end
@@ -11,22 +19,27 @@ class BoardController < ApplicationController
     post = Post.new
     post.title = params[:title]
     post.contents = params[:contents]
+    post.user_id = current_user.id
     post.save
+    #post를 등록할 때 이 글을 작성한 사람은
+    #현재 로그인 되어 있는 유저이다.
+    
     redirect_to "/board/#{post.id}"
   end
   def edit
-    @post = Post.find(params[:id])
   end
   def update
-    post = Post.find(params[:id])
-    post.contents = params[:contents]
-    post.title = params[:title]
-    post.save
+    @post.contents = params[:contents]
+    @post.title = params[:title]
+    @post.save
     redirect_to "/board/#{post.id}"
   end
   def destroy
-    post = Post.find(params[:id])
-    post.destroy
+    @post.destroy
     redirect_to "/boards"
+  end
+  
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
